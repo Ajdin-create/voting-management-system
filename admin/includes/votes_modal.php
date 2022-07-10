8adf64687824054502adafa817f668d11b27b892
Ajdin Jašarević <76125448+Ajdin-create@users.noreply.github.com>
2022-07-10 20:23:01 +0200

    First commit
    For college purpose

---
+++ admin/includes/votes_modal.php
@@ -0,0 +1,22 @@
+<!-- Reset -->
+<div class="modal fade" id="reset">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span></button>
+              <h4 class="modal-title"><b>Resetuj...</b></h4>
+            </div>
+            <div class="modal-body">
+              <div class="text-center">
+                  <p>RESETUJ GLASOVE</p>
+                  <h4>Ova opcija ce izbrisati sve glasove i vratiti ih na nulu.</h4>
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Zatvori</button>
+              <a href="votes_reset.php" class="btn btn-danger btn-flat"><i class="fa fa-refresh"></i> Resetuj</a>
+            </div>
+        </div>
+    </div>
+</div>
